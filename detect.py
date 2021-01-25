94919f606ad9b09a6947f5a5b2b3ad72533d16d7
Bing Liu <bingliu221@gmail.com>
2021-01-25 13:53:50 +0800

    Update detection script

--- detect.py
+++ detect.py
@@ -1,39 +1,52 @@
+import torch
 from torchvision import transforms
-from utils import *
 from PIL import Image, ImageDraw, ImageFont
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+import utils
+from model import SSD300
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+labels = ['background',
+          'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat', 'chair', 'cow', 'diningtable',
+          'dog', 'horse', 'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor']
+
+# Color map for bounding boxes of detected objects from https://sashat.me/2017/01/11/list-of-20-simple-distinct-colors/
+color_map = dict(zip(labels, [None, '#e6194b', '#3cb44b', '#ffe119', '#0082c8', '#f58231', '#911eb4', '#46f0f0', '#f032e6',
+                              '#d2f53c', '#fabebe', '#008080', '#000080', '#aa6e28', '#fffac8', '#800000', '#aaffc3', '#808000',
+                              '#ffd8b1', '#e6beff', '#808080', '#FFFFFF']))
+
 
 # Load model checkpoint
-checkpoint = 'checkpoint_ssd300.pth.tar'
-checkpoint = torch.load(checkpoint)
-start_epoch = checkpoint['epoch'] + 1
-print('\nLoaded checkpoint from epoch %d.\n' % start_epoch)
-model = checkpoint['model']
+checkpoint_path = 'ssd300.pt'
+checkpoint = torch.load(checkpoint_path)
+print('Loaded checkpoint from epoch %d.' % checkpoint.get('epoch', 0))
+model = SSD300(21)
+model.load_state_dict(checkpoint['model_state_dict'])
 model = model.to(device)
 model.eval()
 
 # Transforms
 resize = transforms.Resize((300, 300))
 to_tensor = transforms.ToTensor()
-normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                 std=[0.229, 0.224, 0.225])
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
 
-def detect(original_image, min_score, max_overlap, top_k, suppress=None):
+def detect(image, min_score, max_overlap, top_k, suppress=None):
     """
     Detect objects in an image with a trained SSD300, and visualize the results.
 
-    :param original_image: image, a PIL Image
+    :param image: image, a PIL Image
     :param min_score: minimum threshold for a detected box to be considered a match for a certain class
     :param max_overlap: maximum overlap two boxes can have so that the one with the lower score is not suppressed via Non-Maximum Suppression (NMS)
     :param top_k: if there are a lot of resulting detection across all classes, keep only the top 'k'
     :param suppress: classes that you know for sure cannot be in the image or you do not want in the image, a list
     :return: annotated image, a PIL Image
     """
+    original_image = image
 
     # Transform
-    image = normalize(to_tensor(resize(original_image)))
+    image = normalize(to_tensor(resize(image)))
 
     # Move to default device
     image = image.to(device)
@@ -54,17 +67,16 @@ def detect(original_image, min_score, max_overlap, top_k, suppress=None):
     det_boxes = det_boxes * original_dims
 
     # Decode class integer labels
-    det_labels = [rev_label_map[l] for l in det_labels[0].to('cpu').tolist()]
+    det_labels = [labels[lb] for lb in det_labels[0].to('cpu').tolist() if lb != 0]
 
-    # If no objects found, the detected labels will be set to ['0.'], i.e. ['background'] in SSD300.detect_objects() in model.py
-    if det_labels == ['background']:
-        # Just return original image
+    # If no objects found, Just return original image
+    if len(det_labels) == 0:
         return original_image
 
     # Annotate
     annotated_image = original_image
     draw = ImageDraw.Draw(annotated_image)
-    font = ImageFont.truetype("./calibril.ttf", 15)
+    font = ImageFont.load_default()
 
     # Suppress specific classes, if needed
     for i in range(det_boxes.size(0)):
@@ -72,31 +84,24 @@ def detect(original_image, min_score, max_overlap, top_k, suppress=None):
             if det_labels[i] in suppress:
                 continue
 
+        color = color_map[det_labels[i]]
+
         # Boxes
         box_location = det_boxes[i].tolist()
-        draw.rectangle(xy=box_location, outline=label_color_map[det_labels[i]])
-        draw.rectangle(xy=[l + 1. for l in box_location], outline=label_color_map[
-            det_labels[i]])  # a second rectangle at an offset of 1 pixel to increase line thickness
-        # draw.rectangle(xy=[l + 2. for l in box_location], outline=label_color_map[
-        #     det_labels[i]])  # a third rectangle at an offset of 1 pixel to increase line thickness
-        # draw.rectangle(xy=[l + 3. for l in box_location], outline=label_color_map[
-        #     det_labels[i]])  # a fourth rectangle at an offset of 1 pixel to increase line thickness
+        draw.rectangle(xy=box_location, outline=color)
+        draw.rectangle(xy=[lb + 1. for lb in box_location], outline=color)
 
         # Text
         text_size = font.getsize(det_labels[i].upper())
         text_location = [box_location[0] + 2., box_location[1] - text_size[1]]
-        textbox_location = [box_location[0], box_location[1] - text_size[1], box_location[0] + text_size[0] + 4.,
-                            box_location[1]]
-        draw.rectangle(xy=textbox_location, fill=label_color_map[det_labels[i]])
-        draw.text(xy=text_location, text=det_labels[i].upper(), fill='white',
-                  font=font)
-    del draw
+        textbox_location = [box_location[0], box_location[1] - text_size[1], box_location[0] + text_size[0] + 4., box_location[1]]
+        draw.rectangle(xy=textbox_location, fill=color)
+        draw.text(xy=text_location, text=det_labels[i].upper(), fill='white', font=font)
 
     return annotated_image
 
 
 if __name__ == '__main__':
-    img_path = '/media/ssd/ssd data/VOC2007/JPEGImages/000001.jpg'
-    original_image = Image.open(img_path, mode='r')
-    original_image = original_image.convert('RGB')
-    detect(original_image, min_score=0.2, max_overlap=0.5, top_k=200).show()
+    img_path = './data/VOCdevkit/VOC2007/JPEGImages/000001.jpg'
+    image = Image.open(img_path, mode='r').convert('RGB')
+    detect(image, min_score=0.3, max_overlap=0.5, top_k=200).show()
--- model.py
+++ model.py
@@ -482,7 +482,7 @@ class SSD300(nn.Module):
 
                 # A torch.uint8 (byte) tensor to keep track of which predicted boxes to suppress
                 # 1 implies suppress, 0 implies don't suppress
-                suppress = torch.zeros((n_above_min_score), dtype=torch.uint8).to(device)  # (n_qualified)
+                suppress = torch.zeros((n_above_min_score), dtype=torch.bool).to(device)  # (n_qualified)
 
                 # Consider each box in order of decreasing scores
                 for box in range(class_decoded_locs.size(0)):
@@ -499,9 +499,10 @@ class SSD300(nn.Module):
                     suppress[box] = 0
 
                 # Store only unsuppressed boxes for this class
-                image_boxes.append(class_decoded_locs[1 - suppress])
-                image_labels.append(torch.LongTensor((1 - suppress).sum().item() * [c]).to(device))
-                image_scores.append(class_scores[1 - suppress])
+                not_suppress = torch.logical_not(suppress)
+                image_boxes.append(class_decoded_locs[not_suppress])
+                image_labels.append(torch.LongTensor((not_suppress).sum().item() * [c]).to(device))
+                image_scores.append(class_scores[not_suppress])
 
             # If no object in any class is found, store a placeholder for 'background'
             if len(image_boxes) == 0:
